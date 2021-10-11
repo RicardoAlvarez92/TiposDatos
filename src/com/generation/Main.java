c12bce9620c569139215e951c23958e49e1f21ef
Ricardo Daniel <Ricperez0101@gmail.com>
2021-10-11 17:33:30 -0500

    TIPOS DE DATOS JAVA

---
+++ src/com/generation/Main.java
@@ -0,0 +1,70 @@
+package com.generation;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //	const edad = 31;
+     String nombre = "Ricardo Daniel Alvarez Perez";
+     byte num1 = 10;
+
+     short numShort1 = num1;
+
+     int numInt1 = numShort1;
+
+     short numShort2 = (short)numInt1;
+
+     short num2 = 10;
+     int num3 = 10;
+     long num4 = 10;
+
+//     DATOS PRIMITIVOS DECIMALES
+     float decimal1 = 10.154644f;
+//        a los numeros decimales se les agrega una f al final
+     double decimal2 = 20.58989745369845;
+
+//     DATO PRIMITIVO CHAR
+        char caracter = 'f';
+
+//        DATO PRIMITIVO BOOLEAN
+//        boolean verdadero = true;
+//
+//        byte numByte1 = 15;
+//        byte numByte2 = 5;
+//        byte suma = (byte) (numByte1 + numByte2);
+//        System.out.println(suma);
+//
+//        byte multi = (byte) (numByte1 * numByte2);
+//        System.out.println(multi);
+//
+//        byte divi = (byte) (numByte1 / numByte2);
+//        System.out.println(divi);
+//
+//        byte resta = (byte) (numByte1 - numByte2);
+//        System.out.println(resta);
+//
+//        byte n1 = 5;
+//        byte n2 = 6;
+//
+//        short sum = n1 + n2;
+
+
+
+
+//     System.out.println(nombre);
+//     System.out.println(numShort2);
+//     System.out.println(suma);
+    }
+}
+
+
+//OPERDADORES ARITMETICOS EN JAVA
+//+ SUMAR O COCATENAR CADENAS
+//- RESTAR
+//* MULTIPLICAR
+/// DIVIDIR
+//% RESIDUO
+
+
+
+
+
